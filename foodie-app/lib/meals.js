40b5f49181859d6d2d6c771c0c244132bfac4b77
htetpyiephyoemaung <htetpyiephyoe.maung@kbzbank.com>
2026-06-09 22:55:15 +0630

    feat: share food

--- foodie-app/lib/meals.js
+++ foodie-app/lib/meals.js
@@ -1,12 +1,50 @@
-import sql from 'better-sqlite3';
+import fs from "node:fs";
 
-const db = sql('meals.db');
-export async function getMeals(){
-    // throw new Exception("Error Occurred.");
-    await new Promise((resolve) => setTimeout(resolve, 3000));
-    return db.prepare('Select * from meals').all();
+import sql from "better-sqlite3";
+import slugify from "slugify";
+import xss from "xss";
+
+const db = sql("meals.db");
+
+export async function getMeals() {
+  // throw new Exception("Error Occurred.");
+  await new Promise((resolve) => setTimeout(resolve, 3000));
+  return db.prepare("Select * from meals").all();
 }
 
-export function getMeal(slug){
-    return db.prepare('Select * from Meals where slug =?').get(slug);
+export function getMeal(slug) {
+  return db.prepare("Select * from Meals where slug =?").get(slug);
+}
+
+export async function saveMeal(meal) {
+  meal.slug = slugify(meal.title, { lower: true });
+  meal.instructions = xss(meal.instructions);
+
+  const extension = meal.image.name.split(".").pop();
+  const fileName = `${meal.slug}.${extension}`;
+  const stream = fs.createWriteStream(`public/images/${fileName}`);
+  const bufferedImage = await meal.image.arrayBuffer();
+
+  stream.write(Buffer.from(bufferedImage), (error) => {
+    if (error) {
+      throw new Error("Saving image failed!");
+    }
+  });
+
+  meal.image = `/images/${fileName}`;
+  console.log(meal);
+  db.prepare(
+    `
+        Insert into meals
+        (title, summary, instructions, creator, creator_email, image, slug)
+        Values(
+            @title, 
+            @summary, 
+            @instructions, 
+            @creator, 
+            @creator_email,
+            @image, 
+            @slug)
+    `
+    ).run(meal);
 }
