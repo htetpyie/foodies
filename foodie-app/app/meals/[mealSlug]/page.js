a6d0da09082efab06d0070662de3e5f7d2635a76
htetpyiephyoemaung <htetpyiephyoe.maung@kbzbank.com>
2026-06-09 15:28:08 +0630

    feat: default pages and data pasing to dynamic routes

--- foodie-app/app/meals/[mealSlug]/page.js
+++ foodie-app/app/meals/[mealSlug]/page.js
@@ -1,10 +1,29 @@
-import Link from "next/link";
-export default function SlugPage() {
+import { getMeal } from "@/lib/meals";
+import classes from "./page.module.css";
+import Image from "next/image";
+export default function MealDetailsPage({params}) {
+  const meal = getMeal(params.mealSlug)
+  meal.instructions = meal.instructions.replace(/\n/g, '<br/>');
   return (
     <>
-      <h2>Meal Detail</h2>
-      <Link href="/community">Community</Link>
-      <Link href="/meals/share">Shared</Link>
+      <header className={classes.header}>
+        <div className={classes.image}>
+          <Image src={meal.image} fill />
+        </div>
+        <div className={classes.headerText}>
+          <h1>{meal.title}</h1>
+          <p className={classes.creator}>
+            by <a href={`mailto:${meal.creator_email}`}>{meal.creator}</a>
+          </p>
+          <p className={classes.summary}>{meal.summary}</p>
+        </div>
+      </header>
+      <main>
+        <p
+          className={classes.instructions}
+          dangerouslySetInnerHTML={{ __html: meal.instructions }}
+        ></p>
+      </main>
     </>
   );
 }
--- foodie-app/lib/meals.js
+++ foodie-app/lib/meals.js
@@ -2,6 +2,11 @@ import sql from 'better-sqlite3';
 
 const db = sql('meals.db');
 export async function getMeals(){
-    await new Promise((resolve) => setTimeout(resolve, 5000));
+    // throw new Exception("Error Occurred.");
+    await new Promise((resolve) => setTimeout(resolve, 3000));
     return db.prepare('Select * from meals').all();
+}
+
+export function getMeal(slug){
+    return db.prepare('Select * from Meals where slug =?').get(slug);
 }
